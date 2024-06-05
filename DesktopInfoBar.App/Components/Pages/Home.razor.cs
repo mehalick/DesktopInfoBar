91e620e6334e747365176cfef5981175f216db76
Andy Mehalick <andy.mehalick@outlook.com>
2024-06-05 09:27:31 -0400

    add time display

---
+++ DesktopInfoBar.App/Components/Pages/Home.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DesktopInfoBar.App.Components.Pages;
+
+public partial class Home : ComponentBase
+{
+    private TimeOnly _time = TimeOnly.FromDateTime(DateTime.Now);
+
+    protected override void OnInitialized()
+    {
+        _ = new Timer(_ =>
+        {
+            InvokeAsync(() =>
+            {
+                var now = TimeOnly.FromDateTime(DateTime.Now);
+
+                if (AreSame(_time, now))
+                {
+                    return;
+                }
+
+                _time = now;
+                StateHasChanged();
+            });
+        }, null, 1000, 1000);
+    }
+
+    private static bool AreSame(TimeOnly a, TimeOnly b)
+    {
+        return a.Hour == b.Hour && a.Minute == b.Minute;
+    }
+}
