e2e636d13670cc100dcdc8e145c9f1b082a7d1ec
Andy Mehalick <andy.mehalick@outlook.com>
2024-06-11 14:59:34 -0400

    move to src folder; add publish script; update icon

---
+++ deploy/publish.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$outPath = "$env:LOCALAPPDATA\DesktopInfoBar"
+
+if(!(Test-Path -PathType container $outPath))
+{
+    New-Item -ItemType Directory -Path $outPath
+}
+
+dotnet publish `
+    ..\src\DesktopInfoBar.App\DesktopInfoBar.App.csproj `
+    -f net8.0-windows10.0.19041.0 `
+    -c Release `
+    -p:RuntimeIdentifierOverride=win10-x64 `
+    -p:WindowsPackageType=None `
+    -p:WindowsAppSDKSelfContained=true `
+    --self-contained `
+    --output $outPath
